9bab3b3d10c67bc6fa0435ed1dcdb5316878511c
verdasko <mohammad@alhamoud.de>
2024-12-27 01:08:31 +0100

    .

--- Day10/test.c
+++ Day10/test.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-  char test[] = "";
-  scanf("%s", &test);
+  char test[100] = "";
+  scanf("%s", test);
   while (1) {
     char x = 0;
     for (int i = 0; i < 100; i++) {
